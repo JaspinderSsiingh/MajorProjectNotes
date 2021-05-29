6efedf40e15255ba5e0b222aec00bb734d25e7a2
Jaspinder Singh <jaspindersingh24@gmail.com>
2021-05-29 16:58:33 -0400

    Core Data Project

---
+++ note_Group03_iOS/DataBase/DBManager.swift
@@ -0,0 +1,54 @@
+//
+//  DBManager.swift
+//  note_Group03_iOS
+//
+//  Created by Jaspinder Singh on 17/05/21.
+//
+
+import UIKit
+import SQLite3
+
+let DATABASE_NAME = "Notes.db"
+
+let CATEGORIES_TABLE = "Categories"
+let NOTES_TABLE = "Notes"
+
+var BASE_PATH = ""
+
+class DBManager: NSObject {
+    
+    //MARK:- Shared Instance
+    static let shared = DBManager()
+    private override init() { }
+    
+    //MARK:- Variables
+    var db: OpaquePointer?
+    
+    func getBaseUrl() {
+        let fileURL = try! FileManager.default.url(for: .documentDirectory, in: .userDomainMask, appropriateFor: nil, create: true)
+        BASE_PATH = fileURL.absoluteString.replacingOccurrences(of: DATABASE_NAME, with: "").replacingOccurrences(of: "file://", with: "")
+    }
+    
+    //MARK:- Save file to the Directory
+    func saveImageDocumentDirectory(_ image:UIImage,_ imgName:String) -> String? {
+        let fileManager = FileManager.default
+        let paths = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent("\(imgName)")
+        print(paths)
+        let imageData = image.jpegData(compressionQuality: 1.0)
+        fileManager.createFile(atPath: paths as String, contents: imageData, attributes: nil)
+        return paths
+    }
+    
+
+    // Remove Image
+    func removeImage(_ imgName: String) {
+        let fileManager = FileManager.default
+        let paths = (NSSearchPathForDirectoriesInDomains(.documentDirectory, .userDomainMask, true)[0] as NSString).appendingPathComponent(imgName)
+        if fileManager.fileExists(atPath: paths){
+            try! fileManager.removeItem(atPath: paths)
+        } else {
+            print("Something wrong")
+        }
+    }
+    
+}
---
+++ note_Group03_iOS/Models/CategoryDetailsModel.swift
@@ -0,0 +1,34 @@
+//
+//  CategoryDetailsModel.swift
+//  note_Group03_iOS
+//
+//  Created by Jaspinder Singh on 18/05/21.
+//
+
+import Foundation
+
+struct CategoryDetailsModel {
+    var id: Int!
+    var categoryName: String!
+    var addedDate: String!
+
+    init(_ dict:[String: Any]) {
+        id = dict["id"] as? Int ?? 0
+        categoryName = dict["categoryName"] as? String ?? ""
+        addedDate = dict["addedDate"] as? String ?? ""
+    }
+    
+    func toDict() -> [String: Any] {
+        var dict = [String: Any]()
+        if let id = id {
+            dict["id"] = id
+        }
+        if let categoryName = categoryName {
+            dict["categoryName"] = categoryName
+        }
+        if let addedDate = addedDate {
+            dict["addedDate"] = addedDate
+        }
+        return dict
+    }
+}
